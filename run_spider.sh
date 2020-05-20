f924a59de502a9911bad461e9880e5e38b44cd05
Ron Grimes <ron.grimes@pobox.com>
2020-05-20 11:19:09 -0400

    Simplified - log redirect stuff removed, not used.

--- run_spider.sh
+++ run_spider.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
-LOGFILE="spider_log.txt"
 
 cd ~pi/python/spider
-echo >> $LOGFILE
-echo "-------------------------" >> $LOGFILE
-date >> $LOGFILE
-echo >> $LOGFILE
-sudo ./spider.py & >> $LOGFILE
+sudo ./spider.py &
