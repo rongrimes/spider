446919ddc54144f81e53ae82ad8644215075a2bb
Ron Grimes <ron.grimes@pobox.com>
2023-02-24 17:06:06 -0500

    Restructured to better kill all running processes.

--- kill_spider.sh
+++ kill_spider.sh
@@ -3,17 +3,21 @@
 e=`ps aux | grep spider`
 
 if [ "$(grep -c python3 <<< $e)" -gt 0 ]; then
-    echo Kill key_parms.
-    kill `ps aux | grep python | grep key_parms | tr -s " " | cut -d' ' -f2`
-    sleep 1
-
     echo Kill spider.
     ospid=`cat /home/pi/python/spider/ospid.txt`
     sudo kill -s sigint $ospid
 
     tail --pid=$ospid -f /dev/null              # runs until $ospid goes away
+#        --pid=PID
+#             with -f, terminate after process ID, PID dies
+
+    rm -f ~/python/spider/ospid.txt                  # if it's not there we don't care.
 else
     echo spider not found.
 fi
 
-rm -f ~/python/spider/ospid.txt                  # if it's not there we don't care.
+echo Kill key_parms.
+kill `ps aux | grep python | grep key_parms | tr -s " " | cut -d' ' -f2`
+sleep 1
+
+
